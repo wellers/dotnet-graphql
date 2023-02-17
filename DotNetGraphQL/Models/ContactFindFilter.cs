343beadca2a4c411662284dbb221919574f86f78
wellers <paul_welbourne@yahoo.co.uk>
2023-02-17 16:56:18 +0000

    Update GetContacts with filtering.

---
+++ DotNetGraphQL/Models/ContactFindFilter.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetGraphQL.Models
+{
+	public class ContactFindFilter
+	{
+		public int? Id { get; set; } = null;
+		public string? Title { get; set; } = null;
+		public string? Forename { get; set; } = null;
+		public string? Surname { get; set; } = null;
+	}
+}
--- DotNetGraphQL/Models/Query.cs
+++ DotNetGraphQL/Models/Query.cs
@@ -6,15 +6,27 @@ namespace DotNetGraphQL.Models
 	public sealed class Query
 	{
 		[GraphQLName("find_contacts")]
-		public async Task<ContactFindResult> GetContacts([Service] ContactContext context)
+		public async Task<ContactFindResult> GetContacts([Service] ContactContext context, ContactFindFilter? filter = null)
 		{
-			var contacts = await context.Contacts.ToListAsync();
+			IQueryable<Contact> contacts = context.Contacts;
+			
+			if (filter != null)
+			{
+				contacts = contacts.Where(contact =>
+					(!filter.Id.HasValue || contact.Id == filter.Id)
+					&& (filter.Title == null || contact.Title == filter.Title)
+					&& (filter.Forename == null || contact.Forename == filter.Forename)
+					&& (filter.Surname == null || contact.Surname == filter.Surname)
+				);
+			}
+
+			var docs = await contacts.ToListAsync();
 
 			return new ContactFindResult
 			{
 				Success = true,
 				Message = "Matching records.",
-				Docs = contacts
+				Docs = docs
 			};
 		}
 	}
